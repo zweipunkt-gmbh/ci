31866ff74e3425fb3af547c33cf624372b49565b
kevblu <kevin@zwei.gmbh>
2022-03-23 14:45:29 +0100

    file readout and test on path by phpstan + phpmd

--- run.sh
+++ run.sh
@@ -25,34 +25,42 @@ do
     echo 'File phpstan.neon does not exist'
   fi
 
+  echo "$line"
+  # check if config 2 exists PHPMD
+  if [ ! -e phpmd.xml ]
+  then
+  # file exists run phpmd with memory limit of 4G
+    php -d memory_limit=4G vendor/bin/phpmd $line ansi /src/phpmd.xml
+  else
+  # file does not exist skip the test
+    echo 'File phpmd.xml does not exist'
+  fi
+
 done < "$input"
 
-# check if config 2 exists PHPMD
-if [ ! -e phpmd.xml ]
-then
-# file exists run phpmd with memory limit of 4G
-  php -d memory_limit=4G vendor/bin/phpmd /path/to/code ansi /src/phpmd.xml
-else
-# file does not exist skip the test
-  echo 'File phpmd.xml does not exist'
-fi
+echo "Ab hier sollte PHPCodesniffer übernehmen in einer Schleife"
 
-# check if config 3  exists PHPCodesniffer
-if [ ! -e .phpcs.xml ]
-then
-# file exists run phpcs with memory limit of 4G
-  php -d memory_limit=4G vendor/bin/phpcs --standard=/src/.phpcs.xml
-else
-# file does not exist skip the test
-  echo 'File .phpcs.xml does not exist'
-fi
+#while IFS= read -r line
+#do
+#    # check if config 3  exists PHPCodesniffer
+#    if [ ! -e .phpcs.xml ]
+#    then
+#    # file exists run phpcs with memory limit of 4G
+#      php -d memory_limit=4G vendor/bin/phpcs --standard=/src/.phpcs.xml $line
+#    else
+#    # file does not exist skip the test
+#      echo 'File .phpcs.xml does not exist'
+#    fi
+#done < $input
 
-# check if config 4 exists PHPUnit
-if [ -f "source/phpunit.xml" ];
-then
-# file exists run phpunit with memory limit of 4G
-  php -d memory_limit=4G vendor/bin/phpunit source/public/custom/plugins/ZweiPunktOfferDocument/ZweiPunktOfferDocument.php
-else
-# file does not exist skip the test
-  echo 'File phpunit.xml does not exist'
-fi
+
+
+## check if config 4 exists PHPUnit
+#if [ -f "source/phpunit.xml" ];
+#then
+## file exists run phpunit with memory limit of 4G
+#  php -d memory_limit=4G vendor/bin/phpunit source/public/custom/plugins/ZweiPunktOfferDocument/ZweiPunktOfferDocument.php
+#else
+## file does not exist skip the test
+#  echo 'File phpunit.xml does not exist'
+#fi
