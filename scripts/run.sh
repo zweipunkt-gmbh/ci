67815dcac54e8f3757fc19130d797ce29816bf47
kevblu <kevin@zwei.gmbh>
2022-03-28 15:16:07 +0200

    Anpassungen zum PR-#1 on Github

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,29 +10,31 @@ then
  exit 1
 fi
 
-input="/src/ci.txt"
+# specify the path where the file with the DIR's is located
+# this file should be located in the repositories Root-Dir
+directoryfile="/src/ci.txt"
 
-if [ ! -e $input ]; then
+if [ ! -e $directoryfile ]; then
     echo "No File with paths provided in /src"
     exit 1
 fi
 
-for line in $(cat $input); do
+for line in $(cat $directoryfile); do
   #file exists run phpstan with memory limit of 4G
   php -d memory_limit=4G vendor/bin/phpstan analyse -c ./config/phpstan.neon $line
 done
 
-for line in $(cat $input); do
+for line in $(cat $directoryfile); do
   #file exists run phpmd with memory limit of 4G
   php -d memory_limit=4G vendor/bin/phpmd $line ansi /app/config/phpmd.xml
 done
 
-for line in $(cat $input); do
+for line in $(cat $directoryfile); do
   echo "tester: $line"
   php -d memory_limit=4G vendor/bin/phpcs $line --standard=/app/config/.phpcs.xml
 done
 
-#for line in $(cat $input); do
+#for line in $(cat $directoryfile); do
 #  # file exists run phpunit with memory limit of 4G
 #  php -d memory_limit=4G vendor/bin/phpunit phpunit.xml
 #done
